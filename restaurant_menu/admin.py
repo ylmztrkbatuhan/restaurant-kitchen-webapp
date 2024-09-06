f98162d67a1cc2fa1a7ed82f59ca1937ade2e812
oboy21 <oboy11b@gmail.com>
2024-09-06 16:13:30 +0300

    adding bootstrap

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+    list_filter = ("status",)
+    search_fields = ("meal", "description")
 
-# Register your models here.
+admin.site.register(Item, MenuItemAdmin)
